30fca7364d64da4651b26df6201f7376cbb2bc96
Khitboksy <codydbug@gmail.com>
2026-08-01 00:20:20 -0500

    flake update

--- flake.nix
+++ flake.nix
@@ -28,6 +28,10 @@
             version = "0.1.0";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
+            postInstall = ''
+              mkdir -p $out/share/palette/palettes
+              cp palettes/*.json $out/share/palette/palettes/
+            '';
           };
         }
       );
@@ -50,6 +54,10 @@
           };
         }
       );
+
+      homeManagerModules.default = import ./modules/hm.nix { inherit self; };
+
+      nixosModules.default = import ./modules/nixos.nix { inherit self; };
 
     };
 }
