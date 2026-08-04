21f94150c23c6fc57d2f580702dc059f30cfa78d
Khitboksy <codydbug@gmail.com>
2026-08-04 03:08:09 -0500

    added nix+rust dev tools to shell

--- nix/shell.nix
+++ nix/shell.nix
@@ -4,14 +4,30 @@
 
 pkgs.mkShellNoCC {
   packages = with pkgs; [
+    # Rust
     rustc
     cargo
     clippy
     rustfmt
+    rust-analyzer
     cargo-watch
     gcc
+
+    # Nix
+    nixfmt-tree
+    nixd
+    statix
+    deadnix
   ];
   env = {
     DEV_OPTIONS = "1";
   };
+  shellHook = ''
+    echo "palette-tui dev environment"
+    echo "  rust fmt:   cargo fmt"
+    echo "  rust lsp:   rust-analyzer"
+    echo "  nix fmt:    nixfmt-tree"
+    echo "  nix lsp:    nixd"
+    echo "  nix lint:   statix. deadnix"
+  '';
 }
